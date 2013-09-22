9d7289cad072ee833224269a08fa0368d981bdf8
Kevin Cernekee <cernekee@gmail.com>
2013-09-22 16:20:18 -0700

    res: Add ARM curl binary + android_csd.sh wrapper script
    The curl binary is the precompiled Android release 7.32.0 from
    http://curl.haxx.se/download.html (MIT licensed).
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

---
+++ res/raw/android_csd.sh
@@ -0,0 +1,23 @@
+#!/system/bin/sh
+
+mobile_platform_version="4.3"
+mobile_device_type="Android-x86"
+mobile_device_uniqueid="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+# delete the csdXXXXXX temp file so they don't start piling up
+rm -f $1
+
+exec `dirname $0`/curl \
+	--insecure \
+	--user-agent "AnyConnect Android 3.0.09156" \
+	--header "X-Transcend-Version: 1" \
+	--header "X-Aggregate-Auth: 1" \
+	--header "X-AnyConnect-Identifier-Platform: android" \
+	--header "X-AnyConnect-Identifier-PlatformVersion: $mobile_platform_version" \
+	--header "X-AnyConnect-Identifier-DeviceType: $mobile_device_type" \
+	--header "X-AnyConnect-Identifier-Device-UniqueID: $mobile_device_uniqueid" \
+	--cookie "sdesktop=$CSD_TOKEN" \
+	--data-ascii @- "https://$CSD_HOSTNAME/+CSCOE+/sdesktop/scan.xml" <<-END
+endpoint.feature="failure";
+endpoint.os.version="Android";
+END
