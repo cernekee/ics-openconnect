cernekee/ics-openconnect

commit 4d4c10b02173382a02b8925dae41b897aaf60246

OpenConnectManagementThread: Fix regression on CSD fixup
Commit a543c06cf428 (Allow use of x86 Linux CSD wrapper scripts) added
logic to check for e.g. "#!/bin/sh" at the start of the script, and
prepend "#!/system/bin/sh" if the interpreter was missing.  In a
subsequent commit, d08793cfcb (Copy certs into individual files), this
logic was inadvertently bypassed; stored CSD scripts would be used as-is
instead of rewritten to add the corrected interpreter line.

So, for any stored file for which isExecutable == true, we will copy
the contents to a temporary file and change the interpreter if necessary.
Handling of actual certs (not CSD scripts) should be unaffected.

Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/openconnect/core/OpenConnectManagementThread.java
@@ -296,68 +296,107 @@ public class OpenConnectManagementThread implements Runnable, OpenVPNManagement 
 		return Base64.decode(in, Base64.DEFAULT);
 	}
 
+	private boolean setExecutable(String path) throws IOException {
+		File f = new File(path);
+		if (!f.exists()) {
+			log("PREF: file does not exist");
+			return false;
+		}
+		if (!f.setExecutable(true)) {
+			throw new IOException();
+		}
+		return true;
+	}
+
+	private int writeCertOrScript(String path, String prefData, boolean isExecutable)
+			throws IOException {
+		FileOutputStream fos = new FileOutputStream(path);
+		Writer writer = new BufferedWriter(new OutputStreamWriter(fos, "utf-8"));
+
+		if (isExecutable && rewriteShell(prefData)) {
+			writer.write("#!/system/bin/sh\n");
+		}
+		writer.write(prefData);
+		writer.close();
+
+		if (isExecutable) {
+			setExecutable(path);
+		}
+
+		return prefData.length();
+	}
+
+	private int inlineToTempFile(String path, String prefData, boolean isExecutable)
+			throws IOException {
+		byte data[] = null;
+		int bytes = 0;
+
+		try {
+			FileOutputStream fos = new FileOutputStream(path);
+			data = decodeBase64(prefData);
+			bytes = data.length;
+
+			try {
+				/* Allow reuse of standard x86 Linux CSD scripts */
+				if (isExecutable && rewriteShell(new String(data))) {
+					fos.write("#!/system/bin/sh\n".getBytes());
+				}
+			} catch (Exception e) {
+				/* in case we're trying to pattern-match a binary blob */
+			}
+			fos.write(data);
+			fos.close();
+
+			if (isExecutable) {
+				setExecutable(path);
+			}
+		} catch (IllegalArgumentException e) {
+			/* legacy profiles didn't use base64 encoding */
+			bytes = writeCertOrScript(path, prefData, isExecutable);
+		} catch (IOException e) {
+			return -1;
+		}
+		return bytes;
+	}
+
 	private String prefToTempFile(String prefName, boolean isExecutable) throws IOException {
 		String prefData = getStringPref(prefName);
-		String path;
+		String path = mCacheDir + File.separator + prefName + ".tmp";
 
 		if (prefData.equals("")) {
 			return null;
 		}
 		if (prefData.startsWith(VpnProfile.INLINE_TAG)) {
-			prefData = prefData.substring(10);
-
-			path = mCacheDir + File.separator + prefName + ".tmp";
-
-			FileOutputStream fos = new FileOutputStream(path);
-			byte data[] = null;
-			int bytes = 0;
-
-			try {
-				data = decodeBase64(prefData);
-				bytes = data.length;
-
-				try {
-					/* Allow reuse of standard x86 Linux CSD scripts */
-					if (rewriteShell(new String(data))) {
-						fos.write("#!/system/bin/sh\n".getBytes());
-					}
-				} catch (Exception e) {
-					/* in case we're trying to pattern-match a binary blob */
-				}
-				fos.write(data);
-				fos.close();
-			} catch (IllegalArgumentException e) {
-				/* legacy profiles didn't use base64 encoding */
-				Writer writer = new BufferedWriter(new OutputStreamWriter(fos, "utf-8"));
-
-				if (rewriteShell(prefData)) {
-					writer.write("#!/system/bin/sh\n");
-				}
-				writer.write(prefData);
-				writer.close();
-				bytes = prefData.length();
-			} catch (IOException e) {
+			int bytes = inlineToTempFile(path, prefData.substring(10), isExecutable);
+			if (bytes < 0) {
 				log("PREF: I/O exception writing " + prefName);
 				return null;
+			} else {
+				log("PREF: wrote out " + path + " (" + bytes + ")");
 			}
-			log("PREF: wrote out " + path + " (" + bytes + ")");
 		} else {
+			String srcPath;
+
 			log("PREF: using existing file " + prefData);
 			if (prefData.startsWith("/")) {
-				path = prefData;
+				srcPath = prefData;
 			} else {
-				path = ProfileManager.getCertPath() + prefData;
+				srcPath = ProfileManager.getCertPath() + prefData;
 			}
-		}
 
-		if (isExecutable) {
-			File f = new File(path);
-			if (!f.exists()) {
-				log("PREF: file does not exist");
-				return null;
-			}
-			if (!f.setExecutable(true)) {
-				throw new IOException();
+			if (isExecutable) {
+				/* Make sure that "#!/system/bin/sh" gets prepended to CSD scripts, if needed */
+				String contents = AssetExtractor.readStringFromFile(srcPath);
+				if (contents == null) {
+					return null;
+				}
+				int bytes = writeCertOrScript(path, contents, true);
+				if (bytes < 0) {
+					log("PREF: I/O exception writing " + prefName);
+					return null;
+				} else {
+					log("PREF: wrote out " + path + " (" + bytes + ")");
+				}
 			}
 		}
 		return path;

commit 53023e3e9c527d014759e5c9b50494601e5a1d9a

OpenConnectManagementThread: Fix cert path handling regression
Commit 4d4c10b021733 (Fix regression on CSD fixup) inadvertently left out
the code that assigns the correct path for writing !isExecutable files.
This impacts most/all users who are using certificate authentication.
For more details see:

http://forum.xda-developers.com/showpost.php?p=55201652&postcount=50

Reported-by: Ray-Out@XDA
Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/openconnect/core/OpenConnectManagementThread.java
@@ -397,6 +397,8 @@ public class OpenConnectManagementThread implements Runnable, OpenVPNManagement 
 				} else {
 					log("PREF: wrote out " + path + " (" + bytes + ")");
 				}
+			} else {
+				path = srcPath;
 			}
 		}
 		return path;