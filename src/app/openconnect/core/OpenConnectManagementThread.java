cernekee/ics-openconnect

commit 948a60430ff5e0bf1590cbf6519ebb66ab41524f

OpenConnectManagementThread: Use new Java API to get peer cert
getCertSHA1() has been replaced by getPeerCertHash().

Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/openconnect/core/OpenConnectManagementThread.java
@@ -164,7 +164,7 @@ public class OpenConnectManagementThread implements Runnable, OpenVPNManagement 
 			log("CALLBACK: onValidatePeerCert");
 
 			// This can be called repeatedly on the same (re)connection attempt
-			String hash = getCertSHA1().toLowerCase(Locale.US);
+			String hash = getPeerCertHash().toLowerCase(Locale.US);
 			if (isCertAccepted(hash)) {
 				return 0;
 			}

commit d4429f3cf9c15d2219c39bddffea1dfb9dc44807

OpenConnectManagementThread: Revert back to old SHA1 cert hash
Commit 948a60430ff5e0 ("Use new Java API to get peer cert") inadvertently
changed the format of the certificate hash, which would cause previously
accepted certs to generate a new warning.  The new scheme only hashes a
portion of the cert; the traditional cert fingerprint computes the SHA1
of the entire DER file.

So for now we will manually implement the latter scheme and preserve
compatibility with existing stored hashes.

See also:
http://thread.gmane.org/gmane.network.vpn.openconnect.devel/1956/focus=2113

Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/openconnect/core/OpenConnectManagementThread.java
@@ -30,8 +30,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.security.MessageDigest;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -160,11 +162,34 @@ public class OpenConnectManagementThread implements Runnable, OpenVPNManagement 
     }
 
 	private class AndroidOC extends LibOpenConnect {
+		private String getPeerCertSHA1() {
+			MessageDigest md;
+			try {
+				md = MessageDigest.getInstance("SHA-1");
+			} catch (Exception e) {
+				// if this ever actually happens, the NPE will generate a crash report
+				log("getPeerCertSHA1: could not initialize MessageDigest");
+				return null;
+			}
+
+			md.reset();
+			md.update(getPeerCertDER());
+
+			Formatter f = new Formatter();
+			for (byte b : md.digest()) {
+				f.format("%02X", b);
+			}
+
+			String ret = f.toString();
+			f.close();
+			return ret;
+		}
+
 		public int onValidatePeerCert(String reason) {
 			log("CALLBACK: onValidatePeerCert");
 
 			// This can be called repeatedly on the same (re)connection attempt
-			String hash = getPeerCertHash().toLowerCase(Locale.US);
+			String hash = getPeerCertSHA1().toLowerCase(Locale.US);
 			if (isCertAccepted(hash)) {
 				return 0;
 			}